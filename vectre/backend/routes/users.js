b9c8c10ef3a37ea0072152039a320dff8af41502
Henry <36207470+henryinqz@users.noreply.github.com>
2022-05-20 16:25:59 -0400

    Initialized Express backend

---
+++ vectre/backend/routes/users.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET */
+router.get('/', (req, res, next) => {
+  res.send('GOT a GET request')
+});
+
+/* POST */
+router.post('/', (req, res) => {
+  res.send('Got a POST request')
+  console.log(req.body)
+});
+
+/* PUT */
+router.put('/', (req, res) => {
+  res.send('Got a PUT request')
+  console.log(req.body)
+});
+
+/* DELETE */
+router.delete('/', (req, res) => {
+  res.send('Got a DELETE request')
+  console.log(req.body)
+});
+
+module.exports = router;
