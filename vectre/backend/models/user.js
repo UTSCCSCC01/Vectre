5e83c706abbc3f2bb367abab99632aaeedfd4195
John P <90646213+Onotate@users.noreply.github.com>
2022-06-01 11:18:57 -0400

    created profile editing methods, test commit

---
+++ vectre/backend/models/user.js
@@ -0,0 +1,12 @@
+const _ = require('lodash')
+const User = require('./neo4j/user')
+
+const retrieveProfile = function (wallet) {
+    // Return the profile object of the wallet owner.
+
+}
+
+const updateProfile = function (wallet, changes) {
+    // Update the user profile of the wallet owner using changes.
+    
+} 
