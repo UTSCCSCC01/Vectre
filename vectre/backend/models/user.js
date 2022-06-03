ac48cd85716abf1eec20d7accd49531daff26c8a
John P <90646213+Onotate@users.noreply.github.com>
2022-06-02 23:20:45 -0400

    Created basic methods for retrieving and modifying user object on neo4j.

--- vectre/backend/models/user.js
+++ vectre/backend/models/user.js
@@ -1,12 +1,41 @@
 const _ = require('lodash')
 const User = require('./neo4j/user')
 
-const retrieveProfile = function (wallet) {
-    // Return the profile object of the wallet owner.
+// use auto commit transaction for simplicity.
 
+const getUser = function (session, wallet) {
+    /* Return the user object of the wallet owner.
+    */
+    const findQuery = `MATCH (u:User { wallet_address: ${wallet} }) RETURN u`
+    
+    session
+        .run(findQuery)
+        .then(results => {
+            if (_.isEmpty(results.records)) {
+                return { success: false, user: null, message: "User does not exist"}
+            }
+            else {
+                return { success: true, user: new User(results.records[0]) }
+            }
+        })
 }
 
-const updateProfile = function (wallet, changes) {
-    // Update the user profile of the wallet owner using changes.
+const updateUser = function (session, wallet, newUser) {
+    /* Update the user profile of the wallet owner using newUser.
+     * newUser must be a user object.
+    */
+    const query = `MATCH (u:User { wallet_address: ${wallet} }) SET u = ${newUser} RETURN u`
     
-} 
+    session
+        .run(query)
+        .then(results => {
+            if (_.isEmpty(results.records)) {
+                console.log("Edit user data failed.")
+            }
+        })
+}
+
+module.exports = {
+    getUser : getUser,
+    updateUser : updateUser
+}
