c37bb1b89867689323c236eca38df4fba6292336
peetacho <peterchow2001@gmail.com>
2022-06-03 15:49:40 -0400

    Backend: wrote /create and /getUser

--- vectre/backend/routes/users.js
+++ vectre/backend/routes/users.js
@@ -25,16 +25,58 @@ router.get('/', (req, res, next) => {
 
 /* POST */
 router.post('/create', (req, res) => {
-    const query = `CREATE (user:User {id: '${req.body.id}', name: '${req.body.name}', wallet_address: '${req.body.wallet_address}'})`
+    const query = `CREATE (user:User {name: '${req.body.name}', username: '${req.body.username}', wallet_address: '${req.body.wallet_address}', bio: '${req.body.bio}'});`
     const session = dbUtils.getSession(req);
 
     session.run(query)
-        .then((result) => {
-            res.send(result);
+        .then((results) => {
+            const on_success_data = {
+                success: true,
+                message: "User created.",
+            }
+            res.send(on_success_data);
+        })
+        .catch((error) => {
+            const on_fail_data = {
+                success: false,
+                message: "Failed to create user. Error: " + error,
+            }
+            console.error(error);
+            res.send(on_fail_data);
+        });
+});
+
+/* POST */
+router.post('/getUser', (req, res) => {
+    const query = `MATCH (user:User {wallet_address:"${req.body.wallet_address}"}) RETURN user;`
+    const session = dbUtils.getSession(req);
+    session.run(query)
+        .then((results) => {
+            let response_data = {}
+            results.records.forEach((record) => {
+                response_data = {
+                    success: true,
+                    message: "User wallet_address already exists.",
+                    user_data: new User(record.get('user'))
+                }
+            });
+            if (Object.keys(response_data).length === 0) {
+                response_data = {
+                    success: false,
+                    message: "User wallet_address doesn't exist. Please register your account.",
+                    user_data: null
+                }
+            }
+            res.send(response_data);
         })
         .catch((error) => {
             console.error(error);
-            res.send("Failed to create user. Error: " + error);
+            const response_data = {
+                success: false,
+                message: "User wallet_address doesn't exist. Please register your account.",
+                user_data: null
+            }
+            res.send(response_data);
         });
 });
 
