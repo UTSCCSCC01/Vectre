eb86b60303f02c069479b8f56fef2752b05a8e20
peetacho <peterchow2001@gmail.com>
2022-06-05 10:11:31 -0400

    Frontend: changed /create to /createUser

--- vectre/frontend/src/redux/constants/endpoints.js
+++ vectre/frontend/src/redux/constants/endpoints.js
@@ -3,5 +3,5 @@ export const BASE_API_URL = "http://localhost:8080"
 export const USERS = {
     GET_USER: "/users/getUser",
     GET_USERS: "/users",
-    CREATE_USER: "/users/create"
+    CREATE_USER: "/users/createUser"
 }
