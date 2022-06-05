663f8feca91e012c6076c6ca472682256392b9a7
peetacho <peterchow2001@gmail.com>
2022-06-05 09:46:12 -0400

    Backend: added get/createUser in model + routes

--- vectre/backend/models/user.js
+++ vectre/backend/models/user.js
@@ -3,23 +3,41 @@ const User = require('./neo4j/user')
 
 // use auto commit transaction for simplicity.
 
-const getUser = function (session, wallet) {
+const createUser = function (res, session, body) {
+    /* Creates the user with the following information from body
+    */
+    const query = `CREATE (user:User {name: '${body.name}', username: '${body.username}', wallet_address: '${body.wallet_address}', bio: '${body.bio}'});`
+
+    session.run(query)
+        .then((results) => {
+            res.send({
+                success: true,
+                message: "User created sucessfully.",
+            });
+        })
+        .catch((error) => {
+            res.send({
+                success: false,
+                message: "Failed to create user",
+                error: error.message
+            });
+        });
+}
+
+const getUser = function (res, session, wallet) {
     /* Return the first user object with matching wallet.
     */
-    const findQuery = `MATCH (u: User {wallet_address : \"${wallet}\"}) RETURN u`
-    
-    return session
-        .run(findQuery)
-        .then(results => {
+    const findQuery = `MATCH (user: User {wallet_address : '${wallet}'}) RETURN user`
+    session.run(findQuery)
+        .then((results) => {
             if (_.isEmpty(results.records)) {
-                return { success: false, user: null, message: "User does not exist"}
+                res.send({ success: false, user_data: null, message: "User wallet_address doesn't exist. Please register your account." })
             }
             else {
-                return { success: true, user: new User(results.records[0].get('u')) }
+                res.send({ success: true, message: "User wallet_address already exists.", user_data: new User(results.records[0].get('user')) })
             }
         }).catch((error) => {
-            console.log(error)
-            return { success: false, error: true, user: null, message: "Error while searching"}
+            res.send({ success: false, error: error.message, user_data: null, message: "Error while searching. Please try again." })
         })
 }
 
@@ -30,18 +48,19 @@ const updateUser = function (session, wallet, newUser) {
     let userString = JSON.stringify(newUser)
     let unquoteString = userString.replace(/"([^"]+)":/g, '$1:')
     const modify = `MATCH (u: User {wallet_address: \"${wallet}\"}) SET u = ${unquoteString} RETURN u`
-    
+
     return session
         .run(modify)
         .then(results => {
             if (_.isEmpty(results.records)) {
-                return {success: false}
+                return { success: false }
             }
-            return {success: true}
+            return { success: true }
         })
 }
 
 module.exports = {
-    getUser : getUser,
-    updateUser : updateUser
+    createUser: createUser,
+    getUser: getUser,
+    updateUser: updateUser
 }
