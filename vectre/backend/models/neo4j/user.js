bf6206d4e09362fc139ee618b6a0faa12f0e7929
Henry <36207470+henryinqz@users.noreply.github.com>
2022-06-05 00:12:57 -0400

    Backend: Updated query to create user + simplified User model

--- vectre/backend/models/neo4j/user.js
+++ vectre/backend/models/neo4j/user.js
@@ -1,9 +1,5 @@
 const _ = require("lodash")
 
 const User = module.exports = function (_node)  {
-    _.extend(this, {
-        "id": _node.properties["id"],
-        "name": _node.properties["name"],
-        "wallet_address": _node.properties["wallet_address"],
-    })
+    _.extend(this, _node.properties)
 }
