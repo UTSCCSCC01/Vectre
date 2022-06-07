dd727a4e2be747cfeb914c7f4747eccecd4f8b8d
= <90646213+Onotate@users.noreply.github.com>
2022-06-06 20:44:26 -0400

    VEC-25: Rewrote updateUser to updateProfile, which update name, username, bio of amatching wallet user. Added documentation for both methods.

--- vectre/backend/models/neo4j/user.js
+++ vectre/backend/models/neo4j/user.js
@@ -1,11 +1,5 @@
 const _ = require("lodash")
 
 const User = module.exports = function (_node)  {
-    _.extend(this, {
-        "id": _node.properties["id"],
-        "name": _node.properties["name"],
-        "username": _node.properties["username"],
-        "bio" : _node.properties["username"],
-        "wallet_address": _node.properties["wallet_address"],
-    })
+    _.extend(this, _node.properties)
 }
--- vectre/backend/models/user.js
+++ vectre/backend/models/user.js
@@ -4,8 +4,14 @@ const User = require('./neo4j/user')
 // use auto commit transaction for simplicity.
 
 const getUser = function (session, wallet) {
-    /* Return the first user object with matching wallet.
-    */
+    /**
+     * Return the first user object with matching wallet.
+     * 
+     * @param neo4j session
+     * @param wallet address of the user for searching
+     * @returns an object with a boolean field 'success', field 'user' that holds the user object, and field 'message' if success is false.
+     */
+
     const findQuery = `MATCH (u: User {wallet_address : \"${wallet}\"}) RETURN u`
     
     return session
@@ -19,29 +25,47 @@ const getUser = function (session, wallet) {
             }
         }).catch((error) => {
             console.log(error)
-            return { success: false, error: true, user: null, message: "Error while searching"}
+            return { success: false, user: null, message: "Error while searching"}
         })
 }
 
-const updateUser = function (session, wallet, newUser) {
-    /* Update the user profile of the wallet owner using newUser.
-     * newUser must be a user object similar to how neo4j stores user.
-    */
-    let userString = JSON.stringify(newUser)
-    let unquoteString = userString.replace(/"([^"]+)":/g, '$1:')
-    const modify = `MATCH (u: User {wallet_address: \"${wallet}\"}) SET u = ${unquoteString} RETURN u`
+const updateProfile = function (session, wallet, newProf) {
+    /**
+     * Update the user profile of the wallet owner using newProf object.
+     * 
+     * @param neo4j session.
+     * @param wallet address of the user.
+     * @param object containing new profile, must include following fields: name, username, bio.
+     * @returns an object with a boolean field 'success' and field 'message' if success is false.
+     */
+    
+    const searchUsernameQuery = `MATCH (u: User) 
+        WHERE u.username = \"${newProf.username}\" 
+        AND (NOT u.wallet_address = \"${wallet}\") RETURN u`;
+
+    const modify = `MATCH (u: User {wallet_address: \"${wallet}\"})
+        SET u.name = \"${newProf.name}\", u.username = \"${newProf.username}\", u.bio = \"${newProf.bio}\"
+        RETURN u`;
     
     return session
-        .run(modify)
-        .then(results => {
-            if (_.isEmpty(results.records)) {
-                return {success: false}
+        .run(searchUsernameQuery)       
+        .then(existence => {
+            if ( !_.isEmpty(existence.records)) { // Check for existing username
+                return {success: false, message: "Username already exists."}
+            } else {
+                return session
+                    .run(modify)
+                    .then(results => {
+                        if (_.isEmpty(results.records)) {
+                            return {success: false, message : "Edit failed."}
+                        }
+                        return {success: true}
+                    })
             }
-            return {success: true}
         })
 }
 
 module.exports = {
     getUser : getUser,
-    updateUser : updateUser
+    updateProfile : updateProfile,
 }
