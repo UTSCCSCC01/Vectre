7a5a2e68f5e0d0b9fea8246c6b74939249f30207
Henry <36207470+henryinqz@users.noreply.github.com>
2022-06-10 01:23:30 -0400

    VEC-3: (Frontend) Commented out compilation errors (should be resolved in VEC-27)

--- vectre/frontend/src/pages/EditUserProfilePage/index.js
+++ vectre/frontend/src/pages/EditUserProfilePage/index.js
@@ -1,12 +1,9 @@
-import LandingPageTopComponent from "../../components/LandingPageTopComponent";
 import { Box } from "@chakra-ui/react";
-import LandingPageNavBar from "../../components/LandingPageNavBar";
-import LandingPageFooter from "../../components/LandingPageFooter";
 
 const EditUserProfilePage = () => {
   return (
     <Box>
-      <LandingPageNavBar></LandingPageNavBar>
+      {/*<LandingPageNavBar></LandingPageNavBar>*/}
     </Box>
   );
 };
