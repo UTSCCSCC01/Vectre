0a2deaf3c95efbf0a5967c9fa795b61ba3e2cb37
Nikhil Lakhwani <nikhilnlakhwani@gmail.com>
2022-06-10 17:48:16 -0400

    VEC-33: (Backend) Updated Get/Post for Dashboard

--- vectre/backend/routes/users.js
+++ vectre/backend/routes/users.js
@@ -19,6 +19,13 @@ router.get('/:wallet_address', (req, res) => {
         .catch((error) => res.send(error))
 })
 
+// GET /users/{wallet_address}/dashboard
+router.get('/:wallet_address/dashboard', (req, res) => {
+    User.getDashboard(dbUtils.getSession(req), req.params.wallet_address)
+        .then((result) => res.send(result))
+        .catch((error) => res.send(error))
+})
+
 // POST /users/register
 router.post('/register', (req, res) => {
     User.register(dbUtils.getSession(req), req.body)
@@ -33,40 +40,17 @@ router.post('/login/nonce', (req, res) => {
         .catch((error) => res.send(error))
 })
 
-// GET /users/{wallet_address}/dashboard
-router.get('/:wallet_address/dashboard', (req, res) => {
-    const query =  `MATCH (user:User {wallet_address: "${req.params.wallet_address}" }) RETURN user.dashboard`;
-    const session = dbUtils.getSession(req);
-
-    session.run(query)
-        .then((result) => {
-            res.send(result.records);
-        })
-        .catch((error) => {
-            console.error(error);
-            res.send("Failed to get dashboard. Error: " + error);
-        });
-});
-
-// POST /users/updateDashboard
-router.post('/updateDashboard', (req, res) => {
-    const query = `MATCH (user:User {wallet_address:"${req.body.wallet_address}"}) SET user.dashboard = "${req.body.dashboard}" RETURN user`;
-    const session = dbUtils.getSession(req);
-
-    session.run(query)
-        .then((result) => {
-            res.send(result.records);
-        })
-        .catch((error) => {
-            console.error(error);
-            res.send("Failed to update dashboard. Error: " + error);
-        })
- });
-
 // POST /users/login
 router.post('/login', (req, res) => {
     const setTokenInCookie = (token) => { res.cookie('token', token, { maxAge: 60 * 60 * 24 * 7, httpOnly: true })}
     User.login(dbUtils.getSession(req), req.body.wallet_address, req.body.signed_nonce, setTokenInCookie)
+        .then((result) => res.send(result))
+        .catch((error) => res.send(error))
+})
+
+// POST /users/updateDashboard
+router.post('/updateDashboard', (req, res) => {
+    User.updateDashboard(dbUtils.getSession(req), req.body)
         .then((result) => res.send(result))
         .catch((error) => res.send(error))
 })
