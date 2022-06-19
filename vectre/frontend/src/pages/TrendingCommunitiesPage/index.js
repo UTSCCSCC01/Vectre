f324f3aefda1f9948a7dd07b3daf12e54d396113
peetacho <peterchow2001@gmail.com>
2022-06-19 15:01:34 -0400

    VEC-55: (Frontend) Added AppWrapper

---
+++ vectre/frontend/src/pages/TrendingCommunitiesPage/index.js
@@ -0,0 +1,16 @@
+import {
+  Box,
+} from "@chakra-ui/react";
+import AppWrapper from "../../components/AppWrapper/AppWrapper";
+
+const TrendingCommunitiesPage = () => {
+  return (
+    <AppWrapper>
+      <Box py={'60px'} maxWidth={'4xl'} margin={'0 auto'}>
+        TrendingCommunitiesPage
+      </Box>
+    </AppWrapper>
+  );
+}
+
+export default TrendingCommunitiesPage;
