9fea44b2d27af71212984142eddd70f2c4ab27ad
Henry <36207470+henryinqz@users.noreply.github.com>
2022-07-01 17:04:12 -0400

    VEC-58 (Frontend): Readded postID to Notification component

--- vectre/frontend/src/components/Notifications/Notifications.js
+++ vectre/frontend/src/components/Notifications/Notifications.js
@@ -54,12 +54,12 @@ class Notifications extends React.Component {
   makeNotification(notification) {
     return(
       <Notification
-        fromUser={notification.fromUser}
-        action={notification.action}
-        read={notification.read}
-        link={notification.link}
         key={notification.notificationID}
         notificationID={notification.notificationID}
+        fromUser={notification.fromUser}
+        action={notification.action}
+        postID={notification.postID}
+        read={notification.read}
       />
     )
   }
