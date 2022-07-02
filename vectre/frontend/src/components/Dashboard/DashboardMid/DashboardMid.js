b458ef524fc7c45be99f235ff48e594ebaa1f039
Nikhil Lakhwani <nikhilnlakhwani@gmail.com>
2022-07-02 16:40:32 -0400

    VEC-40: (Frontend) Final Changes

--- vectre/frontend/src/components/Dashboard/DashboardMid/DashboardMid.js
+++ vectre/frontend/src/components/Dashboard/DashboardMid/DashboardMid.js
@@ -4,42 +4,10 @@ import {
     Grid
 } from '@chakra-ui/react';
 
-const dashboardPreset = [
-    {
-        "tokenID": 77809613,
-        "name": "Ryder Ripps Bored Ape Yacht Club",
-        "imageURL": "https://lh3.googleusercontent.com/uCKMr5LZBAfr49-dFuMrWc903x-u4NxBpPywwLjLS9DLMWZDfmjPJW6vlj584SfE2R1YaTgwNd6I6a-Yp90JvoEEpyBd6VhSvf4U",
-        "contractAddress": "0x15545614507f46d954ab1f9c472e26506a99c5f8"
-    },
-    {
-        "tokenID": 77809597,
-        "name": "Ryder Ripps Bored Ape Yacht Club",
-        "imageURL": "https://lh3.googleusercontent.com/HHMN3WLcecCy79ihWzJnUMd-UGlYUTKlRAPues77hRoRlP8B5mbWBS2gvlcqNmiLf_8HCmaVXT1cfX2uS1ATeAg8sATVqvEw4mwa",
-        "contractAddress": "0x15545614507f46d954ab1f9c472e26506a99c5f8"
-    },
-    {
-        "tokenID": 77809576,
-        "name": "Ryder Ripps Bored Ape Yacht Club",
-        "imageURL": "https://lh3.googleusercontent.com/RTvkOSzBsTG0E54t8g4MyXTxETwoIy-91kYLIogGPZx05TX751dRAB7AOSrS74t5Yykay8LuCzy4Ep9UsTaOotYr5lBvpu_oEGoe",
-        "contractAddress": "0x15545614507f46d954ab1f9c472e26506a99c5f8"
-    },
-]
-
-function isJsonString(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-        return false;
-    }
-    return true;
-}
-
 const DashboardMid = ({
     currentDashboard,
 }) => {
     const currentDashboardList = currentDashboard.replace(/'/g, "\"");
-    // console.log(currentDashboardList);
-    // console.log("isJsonSTRING: " + isJsonString(currentDashboardList));
     const dashboard = JSON.parse(currentDashboardList);
 
     return (
