68c083f0941c574e3ef11e3687faef2b81397a35
peetacho <peterchow2001@gmail.com>
2022-07-02 09:21:29 -0400

    VEC-43: (Frontend) sample repost data

--- vectre/frontend/src/pages/HomePage/index.js
+++ vectre/frontend/src/pages/HomePage/index.js
@@ -63,10 +63,25 @@ const samplePostData = [
       profilePic: "https://nextshark.com/wp-content/uploads/2020/07/2skdghdlaiwjkaugsf.jpg",
       verified: true
     },
-    like: "30.2m",
+    likes: "30.2m",
     comment: "43k",
     community: "Meowz",
-    repost: "00001",
+    isRepost: true,
+    repostData: {
+      timestamp: "1:43 PM · May 9, 2022",
+      text: "Saw a dragon today. Wow!",
+      author: {
+        walletAddress: "0x15f209074682937c58ca031ebb43d64fa98d97b8",
+        username: "Deadly Dwarf",
+        profilePic: "https://www.cheatsheet.com/wp-content/uploads/2016/11/tyrion-tywin-1024x683.jpg",
+        verified: true
+      },
+      like: "4.2k",
+      comment: "420",
+      community: "GOT",
+      imageURL: "https://api.time.com/wp-content/uploads/2016/05/tyrion-game-of-thrones-peter-dinklage.jpeg",
+      edited: false
+    },
     edited: false
   }
 ]
