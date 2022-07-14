9a11c42adacc68c5aa6d9c91d09138dc2160dd9b
peetacho <peterchow2001@gmail.com>
2022-07-14 12:12:29 -0400

    VEC-63: (Frontend) cleaned up modal components

--- vectre/frontend/src/components/Modals/CommunityProfileEditModal/CommunityProfileEditModal.js
+++ vectre/frontend/src/components/Modals/CommunityProfileEditModal/CommunityProfileEditModal.js
@@ -3,21 +3,16 @@ import {
     Modal,
     ModalOverlay,
     ModalContent,
-    ModalHeader,
-    ModalCloseButton,
     ModalBody,
     ModalFooter,
-    Button,
-    Flex,
-    Text,
-    Image,
-    Box
+    Button
 } from "@chakra-ui/react"
 import { FaUserFriends } from 'react-icons/fa'
 import { ReactComponent as EditIcon } from "../../../assets/icons/edit-icon.svg";
-import { getAvatarOrDefault, getBannerOrDefault } from "../../../utils/Utils";
 import FormInput from "../FormInput/FormInput";
 import FormTextArea from "../FormTextArea/FormTextArea";
+import StyledModalHeader from "../StyledModalHeader/StyledModalHeader";
+import BannerProfilePicWrapper from "../BannerProfilePicWrapper/BannerProfilePicWrapper";
 
 const CommunityProfileEditModal = ({
     communityData,
@@ -39,31 +34,7 @@ const CommunityProfileEditModal = ({
                 />
                 <ModalContent
                     py={'40px'}>
-                    <ModalHeader
-                        px={{ base: '24px', md: '64px' }}>
-                        <ModalCloseButton
-                            color={'primary.400'}
-                            top={4}
-                            left={4}
-                            transform={'scale(1.8)'}
-                            _focus={{ outline: 0 }}
-                            _hover={{ background: 'white' }}
-                            _active={{ background: 'white' }}
-                        />
-                        <Flex
-                            justifyContent={'center'}
-                            alignItems={'center'}
-                            color={'primary.400'}
-                        >
-                            <Text
-                                fontWeight={700}
-                                fontSize="28px"
-                                mr="15px">
-                                Edit Community
-                            </Text>
-                            <FaUserFriends size={'2rem'} />
-                        </Flex>
-                    </ModalHeader>
+                    <StyledModalHeader headerText={'Edit Community'} icon={<FaUserFriends size={'2rem'} />} />
                     <ModalBody
                         px={{ base: '24px', md: '64px' }}>
                         <form
@@ -83,29 +54,8 @@ const CommunityProfileEditModal = ({
                                 console.log(updatedCommunity)
                             }}
                         >
-                            <Flex
-                                flexDirection={'column'}
-                                gap={'20px'}
-                                position={'relative'}>
-                                <Box
-                                    position={'absolute'}
-                                    top={'53%'}
-                                    left={'50%'}
-                                    marginLeft={'-60px'}>
-                                    <Image
-                                        border={'5px solid white'}
-                                        src={getAvatarOrDefault(communityData.profilePic)}
-                                        fit={'cover'}
-                                        overflow={'hidden'}
-                                        borderRadius={'full'}
-                                        boxSize={'120px'} />
-                                </Box>
-                                <Image
-                                    src={getBannerOrDefault(communityData.banner)}
-                                    fit={'cover'}
-                                    overflow={'hidden'}
-                                    borderRadius={'6px'}
-                                    height={'200px'} />
+                            <BannerProfilePicWrapper
+                                data={communityData}>
                                 <Button
                                     alignSelf={'end'}
                                     ml={'32px'}
@@ -119,20 +69,20 @@ const CommunityProfileEditModal = ({
                                     disabled={true}> {/* TODO: Implement edit avatar/banner */}
                                     Edit
                                 </Button>
-                            </Flex>
+                            </BannerProfilePicWrapper>
                             <FormInput inputID={'name'} inputLabelText={'Name:'} inputDefaultValue={communityData.name} isRequired={true} />
                             <FormInput inputID={'communityID'} inputLabelText={'Community ID:'} inputDefaultValue={communityData.communityID} isRequired={true} />
-                            <FormTextArea inputID={'bio'} inputLabelText={'Bio:'} inputDefaultValue={communityData.bio} />
                             <FormInput inputID={'discordLink'} inputLabelText={'Discord Link:'} inputDefaultValue={communityData.discordLink} />
                             <FormInput inputID={'instagramLink'} inputLabelText={'Instagram Link:'} inputDefaultValue={communityData.instagramLink} />
                             <FormInput inputID={'twitterLink'} inputLabelText={'Twitter Link:'} inputDefaultValue={communityData.twitterLink} />
                             <FormInput inputID={'websiteLink'} inputLabelText={'Website Link:'} inputDefaultValue={communityData.websiteLink} />
                             <FormInput inputID={'ethLink'} inputLabelText={'Etherscan Link:'} inputDefaultValue={communityData.ethLink} />
+                            <FormTextArea inputID={'bio'} inputLabelText={'Bio:'} inputDefaultValue={communityData.bio} />
                         </form>
                     </ModalBody>
                     <ModalFooter
                         pt={'24px'}
-                        px={{ base: '24px', md: '64px' }}>
+                        px={{ base: '24px', md: '74px' }}>
                         <Button
                             type={"submit"}
                             form={"community-setup-form"}
