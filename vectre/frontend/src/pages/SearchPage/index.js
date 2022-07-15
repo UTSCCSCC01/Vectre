1f0f5002962b1b5ed87761dd57e06f68e9b6fac6
peetacho <peterchow2001@gmail.com>
2022-07-15 17:45:46 -0400

    VEC-65: (Frontend) implemented search

--- vectre/frontend/src/pages/SearchPage/index.js
+++ vectre/frontend/src/pages/SearchPage/index.js
@@ -1,26 +1,27 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     Box,
-    Input
+    Input,
+    FormControl
 } from "@chakra-ui/react";
 import ContentWIthNavContainer from "../../components/Containers/ContentWIthNavContainer";
 import TextButton from '../../components/Buttons/TextButton/TextButton';
-import  {HiSearch} from 'react-icons/hi';
+import { HiSearch } from 'react-icons/hi';
 import SearchResultContainer from '../../components/Containers/SearchResult';
-import {useDispatch, useSelector} from "react-redux";
-import {searchUsers} from "../../redux/actions/users";
-import {searchCommunities} from "../../redux/actions/communities";
-import {searchedUsersSelector} from "../../redux/selectors/users";
-import {searchedCommunitiesSelector} from "../../redux/selectors/communities";
+import { useDispatch, useSelector } from "react-redux";
+import { searchUsers } from "../../redux/actions/users";
+import { searchCommunities } from "../../redux/actions/communities";
+import { searchedUsersSelector } from "../../redux/selectors/users";
+import { searchedCommunitiesSelector } from "../../redux/selectors/communities";
 
 const SearchPage = () => {
     const searchedUsers = useSelector(searchedUsersSelector)
     const searchedCommunities = useSelector(searchedCommunitiesSelector)
     const dispatch = useDispatch()
 
-    const searchInput = "E" // TODO: Replace with text field input
+    const [searchInput, setSearchInput] = useState("")
 
-    function handleSearchSubmit() { // TODO: Call on form submit
+    function handleSearchSubmit() {
         dispatch(searchUsers(searchInput))
         dispatch(searchCommunities(searchInput))
     }
@@ -31,31 +32,44 @@ const SearchPage = () => {
     return (
         <ContentWIthNavContainer>
             <Box py={'60px'} maxWidth={'4xl'} margin={'0 auto'}>
-                <form>
-                    <Input backgroundColor={"#FFFFFF"} 
+                <form
+                    id="search-form"
+                    onSubmit={(event) => {
+                        event.preventDefault()
+                        handleSearchSubmit();
+                    }}
+                >
+                    <FormControl>
+                        <Input
+                            id={'search'}
+                            backgroundColor={"white"}
                             px={'17.5px'}
                             fontSize={'18px'}
                             fontWeight={700}
                             color={'primary.400'}
-                            bg={'white'} 
-                            placeholder={"Looking For Something?"}>
-                            </Input>
+                            placeholder={"Looking For Something?"}
+                            _placeholder={{ color: "primary.400" }}
+                            borderRadius={'6px'}
+                            onChange={(event) => { setSearchInput(event.target.value) }} />
+                    </FormControl>
                     <Box display={"flex"} flexDirection={"column"}>
                         <TextButton
-                            rightIcon={<HiSearch/>}
+                            type={'submit'}
+                            form={'search-form'}
                             color={"white"}
                             bg={"primary.400"}
-                            px={"24"}
-                            py={"1"}
                             fontSize={"18px"}
-                            marginTop={"5px"}
-                            marginBottom={"30px"}
+                            px={"24px"}
+                            py={"1px"}
+                            mt={"15px"}
+                            mb={"15px"}
                             fontWeight={700}
                             alignSelf={"flex-end"}
-                            text={"Search"} />
+                            text={"Search"}
+                            rightIcon={<HiSearch />} />
                     </Box>
-                </form>    
-                <SearchResultContainer results={[...searchedUsers, ...searchedCommunities]}/>
+                </form>
+                <SearchResultContainer results={[...searchedUsers, ...searchedCommunities]} />
             </Box>
         </ContentWIthNavContainer>
     );
