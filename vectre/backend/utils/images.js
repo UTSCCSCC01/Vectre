2fd83bf38c7607a315f93625354c96ac09382627
prachyo <prachyosarker565@gmail.com>
2022-07-15 15:52:31 -0400

    updated image upload backend to support base64

--- vectre/backend/utils/images.js
+++ vectre/backend/utils/images.js
@@ -3,6 +3,7 @@ const clientID = config.imgur.clientID
 
 function upload(imageData) {
     const apiURL = 'https://api.imgur.com/3/image';
+    imageData = cleanImageData(imageData);
     return fetch(apiURL, {
         method: 'POST',
         body: imageData,
@@ -34,6 +35,21 @@ function retrieve(body) {
 
 function getHash(imageURL) {
     return imageURL.slice(20).replace(/\.[^/.]+$/, "");
+}
+
+function cleanImageData(imageData) {
+    const pngHeader = "data:image/png;base64,";
+    const jpegHeader = "data:image/jpeg;base64,";
+
+    if (imageData.startsWith(pngHeader)) {
+        imageData = imageData.replace(pngHeader, "");
+    } else if (imageData.startsWith(jpegHeader)) {
+        imageData = imageData.replace(jpegHeader, "");
+    } else {
+        imageData = null;
+    }
+
+    return imageData;
 }
 
 module.exports = {
