4b1b68fad347e9fb2217f0ef5d7969a61b626fa9
peetacho <peterchow2001@gmail.com>
2022-07-27 13:01:36 -0400

    VEC-69: (Frontend) changed ProfileEditModal into functional comp

--- vectre/frontend/src/components/Modals/ProfileEditModal/ProfileEditModal.js
+++ vectre/frontend/src/components/Modals/ProfileEditModal/ProfileEditModal.js
@@ -12,69 +12,73 @@ import FormInput from "../FormInput/FormInput";
 import FormTextArea from "../FormTextArea/FormTextArea";
 import StyledModalHeader from "../StyledModalHeader/StyledModalHeader";
 import BannerProfileEditPicsWrapper from "../BannerProfileEditPicsWrapper/BannerProfileEditPicsWrapper";
-class ProfileEditModal extends React.Component {
 
-    handleProfileEditSubmit = (event) => {
+const ProfileEditModal = ({
+    loggedInUser,
+    updateUser,
+    isOpen,
+    openModal,
+    closeModal
+}) => {
+    const handleProfileEditSubmit = (event) => {
         event.preventDefault();
         let updatedUser = {
             name: event.target.name.value,
             username: event.target.username.value,
             bio: event.target.bio.value
         }
-        this.props.updateUser(updatedUser)
-        this.props.closeModal()
+        updateUser(updatedUser)
+        closeModal()
     }
 
-    render() {
-        return (
-            <>
-                <Modal
-                    isOpen={this.props.isOpen}
-                    onClose={this.props.closeModal}
-                    isCentered
-                    color={'primary.400'}
-                    size={'3xl'}>
-                    <ModalOverlay
-                        bg={'rgba(255, 255, 255, 0.01)'}
-                        backdropFilter='blur(20px)'
-                    />
-                    <ModalContent
-                        py={'40px'}>
-                        <StyledModalHeader headerText={'Edit Profile'} icon={<FaUser size={'2rem'} />} />
-                        <ModalBody
-                            px={{ base: '24px', md: '64px' }}>
-                            <form
-                                id="setup-form"
-                                onSubmit={this.handleProfileEditSubmit}
-                            >
-                                <BannerProfileEditPicsWrapper data={this.props.loggedInUser} />
-                                <FormInput inputID={'name'} inputDefaultValue={this.props.loggedInUser.name} inputLabelText={'Name:'} isRequired={true} />
-                                <FormInput inputID={'username'} inputDefaultValue={this.props.loggedInUser.username} inputLabelText={'Username:'} isRequired={true} />
-                                <FormTextArea inputID={'bio'} inputDefaultValue={this.props.loggedInUser.bio} inputLabelText={'Bio:'} />
-                            </form>
-                        </ModalBody>
-                        <ModalFooter
-                            pt={'24px'}
-                            px={{ base: '24px', md: '64px' }}>
-                            <Button
-                                type={"submit"}
-                                form={"setup-form"}
-                                alignSelf={'end'}
-                                ml={'32px'}
-                                background={'primary.400'}
-                                color={'white'}
-                                px={'46px'}
-                                py={'11px'}
-                                borderRadius={'6px'}
-                                _focus={{ outline: 0 }}>
-                                Save
-                            </Button>
-                        </ModalFooter>
-                    </ModalContent>
-                </Modal>
-            </>
-        );
-    }
+    return (
+        <>
+            <Modal
+                isOpen={isOpen}
+                onClose={closeModal}
+                isCentered
+                color={'primary.400'}
+                size={'3xl'}>
+                <ModalOverlay
+                    bg={'rgba(255, 255, 255, 0.01)'}
+                    backdropFilter='blur(20px)'
+                />
+                <ModalContent
+                    py={'40px'}>
+                    <StyledModalHeader headerText={'Edit Profile'} icon={<FaUser size={'2rem'} />} />
+                    <ModalBody
+                        px={{ base: '24px', md: '64px' }}>
+                        <form
+                            id="setup-form"
+                            onSubmit={handleProfileEditSubmit}
+                        >
+                            <BannerProfileEditPicsWrapper data={loggedInUser} />
+                            <FormInput inputID={'name'} inputDefaultValue={loggedInUser.name} inputLabelText={'Name:'} isRequired={true} />
+                            <FormInput inputID={'username'} inputDefaultValue={loggedInUser.username} inputLabelText={'Username:'} isRequired={true} />
+                            <FormTextArea inputID={'bio'} inputDefaultValue={loggedInUser.bio} inputLabelText={'Bio:'} />
+                        </form>
+                    </ModalBody>
+                    <ModalFooter
+                        pt={'24px'}
+                        px={{ base: '24px', md: '64px' }}>
+                        <Button
+                            type={"submit"}
+                            form={"setup-form"}
+                            alignSelf={'end'}
+                            ml={'32px'}
+                            background={'primary.400'}
+                            color={'white'}
+                            px={'46px'}
+                            py={'11px'}
+                            borderRadius={'6px'}
+                            _focus={{ outline: 0 }}>
+                            Save
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    );
 }
 
 export default ProfileEditModal;
