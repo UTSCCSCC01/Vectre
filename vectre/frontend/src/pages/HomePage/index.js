393fc035a6162d72a7ccaa6d1b8a0b8c592d6674
Henry <36207470+henryinqz@users.noreply.github.com>
2022-07-28 22:23:09 -0400

    VEC-88 (Frontend): Feed loading icon

--- vectre/frontend/src/pages/HomePage/index.js
+++ vectre/frontend/src/pages/HomePage/index.js
@@ -17,6 +17,7 @@ import { useEffect } from "react";
 import { getFeed } from "../../redux/actions/feed";
 import CreatePostComponent from "../../components/CreatePostComponent/CreatePostComponent";
 import SortingButtonComponent from "../../components/SortingButtonComponent/SortingButtonComponent";
+import {showLoading} from "../../redux/actions/global";
 
 const HomePage = () => {
     const sideButtonsList = [
@@ -32,6 +33,7 @@ const HomePage = () => {
         dispatch(getFeed(feedIndex, feedSortType))
     }
     useEffect(() => {
+        dispatch(showLoading(true))
         loadFeed()
     }, [feedSortType])
 
