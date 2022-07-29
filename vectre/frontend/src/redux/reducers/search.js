201835c56d731e799e28f15de1c76fecb90fd3e2
Henry <36207470+henryinqz@users.noreply.github.com>
2022-07-29 00:12:14 -0400

    VEC-89 (Frontend): Filter/sort redux for search

--- vectre/frontend/src/redux/reducers/search.js
+++ vectre/frontend/src/redux/reducers/search.js
@@ -10,12 +10,20 @@ import {
     STORE_SEARCHED_POSTS,
     DO_LIKE_SEARCHED_POST,
     DO_UNLIKE_SEARCHED_POST,
+
+    USERS_COMMUNITIES_FILTER,
+    STORE_SEARCHED_USERS_COMMUNITIES_FILTER,
+    POSTS_SORT_TYPE,
+    STORE_SEARCHED_POSTS_SORT_TYPE,
 } from "../constants/search";
 
 const initialState = {
     users: [],
     communities: [],
-    posts: []
+    posts: [],
+
+    usersCommunitiesFilter: USERS_COMMUNITIES_FILTER.ALL,
+    postsSearchType: POSTS_SORT_TYPE.LIKES
 }
 
 const search = (state = initialState, action) => {
@@ -64,6 +72,23 @@ const search = (state = initialState, action) => {
             return {
                 ...state,
                 posts: state.posts.map((post, i) => post.postID === action.postID ? { ...post, likes: post.likes-1, alreadyLiked: false } : post)
+            }
+        case STORE_SEARCHED_USERS_COMMUNITIES_FILTER:
+            const filter = Object.values(USERS_COMMUNITIES_FILTER).includes(action.filter) ? action.filter : state.usersCommunitiesFilter;
+            return {
+                ...state,
+                usersCommunitiesFilter: filter,
+
+                // Clear users/communities if new filter does not include it
+                users: state.usersCommunitiesFilter !== filter && (filter !== USERS_COMMUNITIES_FILTER.ALL || filter !== USERS_COMMUNITIES_FILTER.USERS) ? initialState.users : state.users,
+                communities: state.usersCommunitiesFilter !== filter && (filter !== USERS_COMMUNITIES_FILTER.ALL || filter !== USERS_COMMUNITIES_FILTER.COMMUNITIES) ? initialState.communities : state.communities
+            }
+        case STORE_SEARCHED_POSTS_SORT_TYPE:
+            const sortType = Object.values(POSTS_SORT_TYPE).includes(action.sortType) ? action.sortType : state.postsSearchType;
+            return {
+                ...state,
+                postsSearchType: sortType,
+                posts: state.postsSearchType !== sortType ? initialState.posts : state.posts // Clear posts if sort type changed
             }
         default:
             return state
