8ce4441c9130ef17afbb11ff554f9435d19f5386
Zhao Ji Wang <mikezerolol20@gmail.com>
2022-07-30 12:43:36 -0400

    fixed more styling of personal community view

--- vectre/frontend/src/components/Modals/PersonalCommunityModal/PersonalCommunityModal.js
+++ vectre/frontend/src/components/Modals/PersonalCommunityModal/PersonalCommunityModal.js
@@ -21,31 +21,40 @@ const PersonalCommunityModal = ({
                 position={'absolute'}
                 py={'16px'}
                 px={'13px'}
-                padding={'20px'}
                 isCentered>
                 <ModalOverlay
                     bg={'rgba(255, 255, 255, 0.01)'}
                     backdropFilter='blur(20px)'
-                />
+                    />
                 <ModalContent
                     height={'500px'}
-                    width={'500px'}
-                    alignItems={'left'}>
+                    width={'600px'}
+                    alignItems={'left'}
+                    padding={'30px'}
+                    bg={'rgba(255, 255, 255, 0.92)'}
+                    gap={'10px'}
+                    >
                     <StyledModalHeader
                         headerText={'My Communities'}
-                        headerFontSize={"18px"}
-                        icon={<IoIosPeople />} />
+                        headerFontSize={"20px"}
+                        icon={<IoIosPeople />} 
+                        overFlowY={'scroll'}/>
                     {communitiesList.map((community, i) =>
                         <EntityCard
                             key={i}
                             primaryText={community.name}
                             secondaryText={"< " + community.communityID + " >"}
                             href={"/c/" + community.communityID}
-                            data={community} >
+                            data={community} 
+                            height = {'10px'}
+                            >
                             <ToggleHollowButton
                                 onText={'Joined'}
                                 offText={'Join'}
                                 isOn={community.alreadyJoined}
+                                bg={'#3B82F68F'}
+                                color={'white'}
+                                border={'none'}
                                 onClick={() => console.log("Peter was here")} />
                         </EntityCard>
                     )}
