52bcde6b6855e3bb7310532a1655ef46fcb407a8
Henry <36207470+henryinqz@users.noreply.github.com>
2022-07-29 22:43:06 -0400

    VEC-79 (Frontend): Added trending header

--- vectre/frontend/src/components/Trending/Trending.js
+++ vectre/frontend/src/components/Trending/Trending.js
@@ -3,6 +3,7 @@ import SearchResultContainer from "../Search/SearchResult/SearchResult";
 import {useDispatch, useSelector} from "react-redux";
 import {trendingCommunitiesSelector} from "../../redux/selectors/trending";
 import {getTrendingCommunities} from "../../redux/actions/trending";
+import {Box} from "@chakra-ui/react";
 
 const Trending = () => {
     const trendingCommunities = useSelector(trendingCommunitiesSelector)
@@ -14,7 +15,37 @@ const Trending = () => {
 
 
     return (
-        <SearchResultContainer results={trendingCommunities} />
+        <>
+            <Box
+                bg={"#FFFFFFAB"}
+                gap={"5px"}
+                borderRadius={"6px"}
+                px={"24px"}
+                py={"1px"}
+                padding={"10px 25px"}
+                display={"flex"}
+                flexDirection={"row"}
+                alignItems={"center"}
+                justifyContent={"center"}
+                fontSize={"23px"}
+                fontFamily={"DM Sans"}
+                fontWeight={700}
+                color={"primary.400"}>
+                Trending
+                {/*<BsGlobe />*/}
+            </Box>
+            <br />
+            {/*<Box width={"100%"} display={"grid"} gridTemplateColumns={"1fr 1fr"} gridGap="24px">*/}
+            {/*    {results.map((result, i) => {*/}
+            {/*        return (*/}
+            {/*            <IndividualSearchResult key={i} result={result} />*/}
+            {/*        )*/}
+            {/*    })}*/}
+            {/*</Box>*/}
+
+            <SearchResultContainer results={trendingCommunities} />
+        </>
+
     )
 }
 
